27982d446fe056920c4e43e817d31557e7b0addb
SmsS4 <smss.lite@gmail.com>
2024-04-20 17:36:47 +0330

    Add link to SyncRepository in db

--- fase/db/__init__.py
+++ fase/db/__init__.py
@@ -2,3 +2,4 @@ from fase.db import base, connection, repository
 from fase.db.base import Base, TimeStamp, ClassNameAsTableName
 from fase.db.connection import session
 from fase.db.repository import Repository
+from fase.db.sync_repository import SyncRepository
