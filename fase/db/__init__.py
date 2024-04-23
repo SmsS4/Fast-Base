6b3bdf15b2d4736cd48afb21dbd479224f4a41b3
SmsS4 <smss.lite@gmail.com>
2024-04-23 09:26:11 +0330

    Add deps to db __init__

--- fase/db/__init__.py
+++ fase/db/__init__.py
@@ -3,3 +3,4 @@ from fase.db.base import Base, TimeStamp, ClassNameAsTableName
 from fase.db.connection import session
 from fase.db.repository import Repository
 from fase.db.sync_repository import SyncRepository
+from fase.db import deps
