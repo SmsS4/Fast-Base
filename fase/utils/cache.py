b49876a0fb89f31e5748510163989791a486b9e6
SmsS4 <smss.lite@gmail.com>
2024-05-06 14:56:18 +0330

    Add default for cache

--- fase/utils/cache.py
+++ fase/utils/cache.py
@@ -1,11 +1,19 @@
 import time
 from datetime import timedelta
+from typing import Any
 from typing import Generic
 from typing import TypeVar
 
 import fastapi
 
 T = TypeVar("T")
+
+
+class __UNSET:
+    pass
+
+
+UNSET = __UNSET()
 
 
 class Cache(Generic[T]):
@@ -16,14 +24,18 @@ class Cache(Generic[T]):
     def put(self, key: str, value: T) -> None:
         self.data[key] = (value, time.time())
 
-    def get(self, key: str) -> T | None:
+    def get(self, key: str, default: Any = UNSET) -> T:
         value_time = self.data.get(key, None)
         if value_time is None:
-            return None
+            if default != UNSET:
+                return default
+            raise KeyError(key)
         value, create_time = value_time
         if self.ttl and time.time() - create_time > self.ttl:
             self.data.pop(key)
-            return None
+            if default != UNSET:
+                return default
+            raise KeyError(key)
         return value
 
     def dep(self):
